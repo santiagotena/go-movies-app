e9353449146513909fcd6395b0aaf4fbd1a8083f
Santiago Tena Hernandez <stenahe@gmail.com>
2024-01-26 13:17:37 +0100

    feat: Add base for homepage

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,36 @@
-import React, { Fragment } from "react";
+import Home from "./components/Home";
+import "./App.css";
 
 function App() {
-	return (
-		<></>
-	);
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1 className="mt-3">Go watch a movie</h1>
+        </div>
+        <div className="col text-end">
+          <a href="#!">
+            <span className="badge bg-success">Login</span>
+          </a>
+        </div>
+        <hr className="mb-3"></hr>
+      </div>
+
+      <div className="row">
+        <div className="col-md-2">
+          <nav>
+            <div className="list-group">
+              <a href="#!" className="list-group-item list-group-item-action">Home</a>
+              <a href="#!" className="list-group-item list-group-item-action">Movies</a>
+            </div>
+          </nav>
+        </div>
+        <div className="col-md-10">
+          <Home></Home>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
