dc795ac0cac021811c088f6a72029c4bc2a15352
Santiago Tena Hernandez <stenahe@gmail.com>
2024-01-26 17:28:41 +0100

    feat: add base for jwt token

--- app/src/components/Movie.js
+++ app/src/components/Movie.js
@@ -1,15 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 function Movie() {
-	const [movie, setMovie] = useState({});
+  const [movie, setMovie] = useState({});
+  let { id } = useParams();
 
+  useEffect(() => {
+    let myMovie = {
+      id: 1,
+      title: "Highlander",
+      release_date: "1986-03-07",
+      runtime: 116,
+      mpaa_rating: "R",
+      description: "Some long description",
+    };
+    setMovie(myMovie);
+  }, [id]);
 
-	return(
-		<div className="text-center">
-			<h2>Movie</h2>
-			<hr></hr>
-		</div>
-	)
+  return (
+    <div className="text-center">
+      <h2>Movie: {movie.title}</h2>
+			<small><em>{movie.release_date}, {movie.runtime} minutes, Rated {movie.mpaa_rating}</em></small>
+      <hr></hr>
+      <p>{movie.description}</p>
+    </div>
+  );
 }
 
 export default Movie;
