a4d5c69df101fa82aeb8ab0f15b403b9bafceee2
Santiago Tena Hernandez <stenahe@gmail.com>
2024-01-26 16:59:46 +0100

    feat: add link to movies with image click

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,17 +1,18 @@
-import Ticket from './../images/movie_tickets.jpg'
+import { Link } from "react-router-dom";
+import Ticket from "./../images/movie_tickets.jpg";
 
 function Home() {
-
-	return(
-		<>
-		<div className="text-center">
-			<h2>Find a movie to watch tonight!</h2>
-			<hr></hr>
-			<img src={Ticket} alt="movie tickets"></img>
-		</div>
-		</>
-	)
-
+  return (
+    <>
+      <div className="text-center">
+        <h2>Find a movie to watch tonight!</h2>
+        <hr></hr>
+        <Link to={"./movies"}>
+          <img src={Ticket} alt="movie tickets"></img>
+        </Link>
+      </div>
+    </>
+  );
 }
 
 export default Home;
