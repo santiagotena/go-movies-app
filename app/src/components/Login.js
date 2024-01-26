16eba370549362aa7fe4c2370f1e0b75811dd829
Santiago Tena Hernandez <stenahe@gmail.com>
2024-01-26 17:55:06 +0100

    feat: add base for login

--- app/src/components/Login.js
+++ app/src/components/Login.js
@@ -1,11 +1,43 @@
-function Login() {
+import { useState } from "react";
+import Input from "./form/Input";
 
-	return(
-		<div className="text-center">
-			<h2>Login</h2>
-			<hr></hr>
-		</div>
-	)
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <div className="col-md-6 offset-md-3">
+      <h2>Login</h2>
+      <hr></hr>
+
+      <form onSubmit={handleSubmit}>
+        <Input
+          type="Email Address"
+          className="email"
+          name="email"
+          autoComplete="email-new"
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <Input
+          type="Password"
+          className="password"
+          name="password"
+          autoComplete="password-new"
+          onChange={(event) => setPassword(event.target.value)}
+        />
+				<hr></hr>
+				<input
+					type="submit"
+					className="btn btn-primary"
+					value="Login"
+				></input>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
