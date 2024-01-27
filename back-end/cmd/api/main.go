7e3689674d33a4c6380e54caa7079df1ff836dbf
Santiago Tena Hernandez <stenahe@gmail.com>
2024-01-27 13:45:10 +0100

    feat: connect to database

--- back-end/cmd/api/main.go
+++ back-end/cmd/api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,7 +11,9 @@ import (
 const port = 8080
 
 type application struct {
+	DSN string
 	Domain string
+	DB *sql.DB
 }
 
 func main(){
@@ -17,14 +21,22 @@ func main(){
 	var app application
 
 	//read from command line
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC connect_timeout=5", "Postgres connection string")
+	flag.Parse()
 
 	//connect to the DB
+	conn, err := app.connectToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	app.DB = conn
+
 	app.Domain = "example.com"
 
 	log.Println("Starting application on port", port)
 
 	//start a web server
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
